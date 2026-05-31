1bc3fb0d3ee1db466acd0d7954aa4ea11f84be98
Manuel Gonzalez <gonzalezjuanmanuel645@gmail.com>
2026-05-31 00:17:59 -0500

    fix: corriguendo variables de entorno

--- apps/api/main.py
+++ apps/api/main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="Mariana's - Monitor Judicial", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "https://mariana-app-nu.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
